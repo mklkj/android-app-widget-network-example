ce10a96155fd053d1e13cd2f46a4dd9e48421d65
Mikołaj Pich <m.pich@outlook.com>
2021-04-04 16:33:10 +0200

    Reload widget content on reload button click

--- app/src/main/java/io/github/mklkj/androidappwidgetexample/ui/randomwidget/RandomWidgetProvider.kt
+++ app/src/main/java/io/github/mklkj/androidappwidgetexample/ui/randomwidget/RandomWidgetProvider.kt
@@ -2,7 +2,9 @@ package io.github.mklkj.androidappwidgetexample.ui.randomwidget
 
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_CANCEL_CURRENT
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
@@ -30,6 +32,27 @@ class RandomWidgetProvider : AppWidgetProvider(), CoroutineScope {
     @Inject
     lateinit var wikipediaRepository: WikipediaRepository
 
+    companion object {
+        private const val BUTTON_TYPE_KEY = "button_type_key"
+        private const val BUTTON_REFRESH_KEY = "button_refresh_key"
+        private const val BUTTON_RELOAD_KEY = "button_reload_key"
+        private const val WIDGET_ID_KEY = "widget_id_key"
+
+        private const val WIDGET_ID_DEFAULT = -1
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        val widgetId = intent?.getIntExtra(WIDGET_ID_KEY, WIDGET_ID_DEFAULT)
+        if (widgetId != WIDGET_ID_DEFAULT && intent?.action == ACTION_APPWIDGET_UPDATE) {
+            launch {
+                val remoteViews = RemoteViews(context?.packageName, R.layout.appwidget_random)
+                remoteViews.onWidgetUpdate(context, widgetId!!)
+                AppWidgetManager.getInstance(context).updateAppWidget(widgetId, remoteViews)
+            }
+        }
+    }
 
     override fun onUpdate(
         context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray
@@ -37,13 +60,13 @@ class RandomWidgetProvider : AppWidgetProvider(), CoroutineScope {
         appWidgetIds.forEach {
             val remoteViews = RemoteViews(context.packageName, R.layout.appwidget_random)
             launch {
-                remoteViews.onWidgetUpdate(context)
+                remoteViews.onWidgetUpdate(context, it)
                 appWidgetManager.updateAppWidget(it, remoteViews)
             }
         }
     }
 
-    private suspend fun RemoteViews.onWidgetUpdate(context: Context) {
+    private suspend fun RemoteViews.onWidgetUpdate(context: Context?, widgetId: Int) {
         val page = wikipediaRepository.getCachedPageRandomSummary()
         setTextViewText(R.id.app_name, page?.title)
 
@@ -55,13 +78,22 @@ class RandomWidgetProvider : AppWidgetProvider(), CoroutineScope {
         setOnClickPendingIntent(R.id.app_name, Intent(context, MainActivity::class.java).let {
             PendingIntent.getActivity(context, 0, it, 0)
         })
-        // todo: refresh from network
-        setOnClickPendingIntent(R.id.refresh_button, Intent(context, MainActivity::class.java).let {
-            PendingIntent.getActivity(context, 0, it, 0)
-        })
-        // todo: reload from sharedpref
-        setOnClickPendingIntent(R.id.reload_button, Intent(context, MainActivity::class.java).let {
-            PendingIntent.getActivity(context, 0, it, 0)
-        })
+        setOnClickPendingIntent(
+            R.id.refresh_button, context?.buttonIntent(0, widgetId, BUTTON_REFRESH_KEY)
+        )
+        setOnClickPendingIntent(
+            R.id.reload_button, context?.buttonIntent(0, widgetId, BUTTON_RELOAD_KEY)
+        )
+    }
+
+    private fun Context.buttonIntent(code: Int, id: Int, buttonType: String): PendingIntent {
+        return PendingIntent.getBroadcast(
+            this, code,
+            Intent(this, RandomWidgetProvider::class.java).apply {
+                action = ACTION_APPWIDGET_UPDATE
+                putExtra(BUTTON_TYPE_KEY, buttonType)
+                putExtra(WIDGET_ID_KEY, id)
+            }, FLAG_UPDATE_CURRENT
+        )
     }
 }
